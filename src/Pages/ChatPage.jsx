6eba1e289dfc8b55d1646ecbd3dee741db5d9ed4
Rafay0237 <143127689+Rafay0237@users.noreply.github.com>
2024-05-17 01:03:12 +0500

    send messages and key event listener

--- src/Pages/ChatPage.jsx
+++ src/Pages/ChatPage.jsx
@@ -4,7 +4,7 @@ import FreindProfileChatBar from "../Components/FreindProfileChatBar";
 import Messages from "../Components/Messages";
 import SendMessage from "../Components/SendMessage";
 import { useSelector } from "react-redux";
-import { getData } from "../APICALLS";
+import { getData, submitData } from "../APICALLS";
 import { io } from "socket.io-client";
 
 const ChatPage = () => {
@@ -35,13 +35,24 @@ const ChatPage = () => {
     });
 
     socket.current.on("getImage", (data) => {
-      console.log(data)
       setArrivalMessage({
         sender: data.senderId,
         img: data.img,
         createdAt: Date.now(),
       });
     });
+
+    socket.current.on("updateMessageSeen",(data)=>{
+      console.log(data)
+      let updatedMessages = messages.map(message => {
+        if (message.conversationId === data.conversationId && message.sender === data.sender) {
+          return { ...message, seen: true }; 
+        } else {
+          return message; 
+        }
+      });
+      setMessages(updatedMessages)
+    })
   }, []);
 
   useEffect(() => {
@@ -71,10 +82,21 @@ const ChatPage = () => {
   }, []);
 
   useEffect(() => {
-    currentChat &&
+      if(currentChat){
       getData("chat/messages/" + currentChat._id).then((data) => {
         setMessages(data.messageList);
       });
+      const freindId =currentChat.members.find((m) => m !== currentUser.user._id);
+      submitData("chat/messages/update-seen","PUT",{
+      conversationId:currentChat._id,
+      sender:freindId
+      })
+
+      socket.current.emit("messageSeen",{
+        conversationId:currentChat._id,
+        sender:freindId
+      })
+    }
   }, [currentChat]);
 
   useEffect(() => {
