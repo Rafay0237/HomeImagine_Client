f6f4997a7faa5f097361f99beac323023c386b54
Rafay0237 <143127689+Rafay0237@users.noreply.github.com>
2024-06-08 16:30:32 +0500

    minor Changes

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -8,20 +8,20 @@ const Slider = ({images}) => {
   return (
     <div className='flex relative '>
 
-        <button className='rounded-full bg-white shadow-md  p-2 absolute left-5 top-[50%] '
-      onClick={()=>currentImage===0?setCurrentImage(3):setCurrentImage(currentImage-1)}
+        <button className='rounded-full bg-white shadow-md  p-2 absolute left-0 sm:left-5 top-[45%] '
+      onClick={()=>currentImage===0?setCurrentImage(images.length-1):setCurrentImage(currentImage-1)}
       ><GrFormPrevious className='text-3xl text-dark-grey'/></button>
 
-      {images.map(img=>(
+      {images?.map(img=>(
           <img 
-          className={'object-contain w-[90%] mx-auto rounded-md shadow-lg'
+          className={'object-contain h-[55vh] w-[90%] mx-auto rounded-md shadow-lg'
           +(images[currentImage]!=img&&" hidden")}
           src={img} alt='Project Wallparers'
           key={img} />
           ))}
 
-      <button className='rounded-full bg-white  p-2 shadow-lg absolute right-5  top-[50%] '
-      onClick={()=>currentImage===3?setCurrentImage(0):setCurrentImage(currentImage+1)}
+      <button className='rounded-full bg-white  p-2 shadow-lg absolute right-0 sm:right-5  top-[45%] '
+      onClick={()=>currentImage===images.length-1?setCurrentImage(0):setCurrentImage(currentImage+1)}
       ><GrFormNext className='text-3xl text-dark-grey'/></button>
 
       </div>
