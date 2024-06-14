c5df8738a15f1be3557d541f4d30863b10a1a83e
Rafay0237 <143127689+Rafay0237@users.noreply.github.com>
2024-06-15 01:44:20 +0500

    Oauth updated

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { store,persistor } from './redux/store.js';
 import { Provider } from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react'
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { GoogleOAuthProvider } from '@react-oauth/google'; 
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,9 +20,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_APP_OAUTH_CLIENT_ID}>
       <QueryClientProvider client={queryClient}>
     <App />
       </QueryClientProvider>
+      </GoogleOAuthProvider>;
     </PersistGate>
   </Provider>
 );
